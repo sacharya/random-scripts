c32e790689d07cbf5d4251367d9b266068e36eb5
sacharya <info@sacharya.com>
2013-03-07 15:20:03 -0600

    Added comments

--- chef/chef-knife-install.sh
+++ chef/chef-knife-install.sh
@@ -2,11 +2,13 @@
 
 set -x
 
+# Make sure the Openstack Credentials are set.
 : ${OPENSTACK_USER:?"Need to set OPENSTACK_USER non-empty"}
 : ${OPENSTACK_PASSWORD:?"Need to set OPENSTACK_PASSWORD non-empty"}
 : ${OPENSTACK_TENANT:?"Need to set OPENSTACK_TENANT non-empty"}
 : ${OPENSTACK_REGION:?"Need to set OPENSTACK_REGION non-empty"}
 : ${OPENSTACK_AUTH_URL:?"Need to set OPENSTACK_AUTH_URL non-empty"}
+
 
 apt-get update
 
@@ -17,14 +19,9 @@ AMQP_PASSWORD=${AMQP_PASSWORD:-$(pwgen -1)}
 WEBUI_PASSWORD=${WEBUI_PASSWORD:-$(pwgen -1)}
 
 echo "deb http://apt.opscode.com/ `lsb_release -cs`-0.10 main"| sudo tee /etc/apt/sources.list.d/opscode.list
-
-
 echo "deb http://apt.opscode.com/ precise-0.10 main" | sudo tee /etc/apt/sources.list.d/opscode.list
-
 sudo mkdir -p /etc/apt/trusted.gpg.d
-
 gpg --keyserver keys.gnupg.net --recv-keys 83EF826A
-
 gpg --export packages@opscode.com|sudo tee /etc/apt/trusted.gpg.d/opscode-keyring.gpg >/dev/null
 
 cat <<EOF | debconf-set-selections
@@ -34,15 +31,11 @@ chef-server-webui chef-server-webui/admin_password password ${WEBUI_PASSWORD}
 EOF
 
 apt-get update
-
 apt-get install -y --force-yes opscode-keyring
-
 apt-get upgrade -y --force-yes
-
 apt-get install -y --force-yes chef chef-server
 
 SUDO_USER=root
-
 HOMEDIR=$(getent passwd ${SUDO_USER} | cut -d: -f6)
 mkdir -p ${HOMEDIR}/.chef
 cp /etc/chef/validation.pem /etc/chef/webui.pem ${HOMEDIR}/.chef
@@ -59,7 +52,7 @@ ${HOMEDIR}/.chef/validation.pem
 
 EOF
 
-
+# Grab the cookbooks and upload them to chef-server
 apt-get -y install git-core
 git clone https://github.com/rackspace/knife-alamo.git
 git clone https://github.com/rackspace/hdp-cookbooks.git
@@ -70,6 +63,8 @@ EOF
 
 knife cookbook upload -a
 
+# Grab knife-alamo and install it
+git clone https://github.com/rackspace/knife-alamo.git
 apt-get install -y ruby ruby-dev libopenssl-ruby rdoc ri irb build-essential wget ssl-cert curl
 curl -O http://production.cf.rubygems.org/rubygems/rubygems-1.8.10.tgz
 tar zxf rubygems-1.8.10.tgz
